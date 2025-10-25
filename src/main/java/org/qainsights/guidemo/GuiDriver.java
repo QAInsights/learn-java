da522db51a1e3b585821a4c3fa66112c8f10c390
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2025-10-25 18:30:56 -0400

    Update

--- src/main/java/org/qainsights/guidemo/GuiDriver.java
+++ src/main/java/org/qainsights/guidemo/GuiDriver.java
@@ -3,11 +3,9 @@ package org.qainsights.guidemo;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class GuiDriver implements ActionListener  {
+public class GuiDriver implements ActionListener {
 
-    private JFrame jFrame;
     private JButton jButton; // instance variable
-    private JButton resetButton;
 
     private int counter;
 
@@ -19,20 +17,19 @@ public class GuiDriver implements ActionListener  {
     }
 
     private void go() {
-        jFrame = new JFrame();
+        JFrame jFrame = new JFrame();
         jButton = new JButton("OK"); // local variable
-        resetButton = new JButton("Reset");
+        JButton resetButton = new JButton("Reset");
 
-         // Add both buttons to the frame
+        // Add both buttons to the frame
         JPanel panel = new JPanel();
         panel.add(jButton);
         panel.add(resetButton);
 
 
-
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.getContentPane().add(panel);
- 
+
 
         jButton.addActionListener(this);
         resetButton.addActionListener(this);
@@ -47,13 +44,12 @@ public class GuiDriver implements ActionListener  {
     public void actionPerformed(ActionEvent e) {
         jButton.setText("Clicked " + ++counter);
 
-        if ("Reset".equals(e.getActionCommand())){
+        if ("Reset".equals(e.getActionCommand())) {
             System.out.println("Reset");
             jButton.setText("OK");
             counter = 0;
         }
     }
 
-    
 
 }
